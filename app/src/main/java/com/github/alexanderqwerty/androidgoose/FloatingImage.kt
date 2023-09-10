e53e4cef97673e5dec1d235e07cde305f0f48e08
Alex <duducensash@gmail.com>
2023-09-10 11:56:51 +0300

    Fix bug with height of screen

--- app/src/main/java/com/github/alexanderqwerty/androidgoose/FloatingImage.kt
+++ app/src/main/java/com/github/alexanderqwerty/androidgoose/FloatingImage.kt
@@ -1,8 +1,9 @@
 package com.github.alexanderqwerty.androidgoose
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
-import android.graphics.Color.red
+import android.graphics.Insets
 import android.graphics.PixelFormat
 import android.graphics.Rect
 import android.os.Build
@@ -10,11 +11,13 @@ import android.util.DisplayMetrics
 import android.util.Log
 import android.view.Gravity
 import android.view.MotionEvent
+import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
+
 
 @RequiresApi(Build.VERSION_CODES.R)
 @SuppressLint("ClickableViewAccessibility")
@@ -32,7 +35,7 @@ class FloatingImage(context: Context) {
     init {
         // Настройте изображение и его размеры здесь
         imageView.setImageResource(R.drawable.ic_launcher_foreground)
-//        imageView.setBackgroundColor(ContextCompat.getColor(context, R.color.purple_200))
+        imageView.setBackgroundColor(ContextCompat.getColor(context, R.color.purple_200))
 
         textView.text = "0"
         val params = WindowManager.LayoutParams(
@@ -43,10 +46,14 @@ class FloatingImage(context: Context) {
             PixelFormat.TRANSLUCENT
         )
         val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+//        val screenWidth = rootView.width
+//        val screenHeight = rootView.height
         displayMetrics = windowManager.currentWindowMetrics.bounds
-        screenWidth = displayMetrics.width()
-        screenHeight = displayMetrics.height()
+
+        screenWidth = context.display?.width!!.toInt()
+        screenHeight = context.display?.height!!.toInt()
         Log.i("FloatingImage", "screenWidth: $screenWidth screenHeight: $screenHeight")
+
         params.gravity = Gravity.TOP or Gravity.START
         windowManager.addView(imageView, params)
         windowManager.addView(textView, params)
@@ -57,7 +64,7 @@ class FloatingImage(context: Context) {
                     lastX = event.rawX.toInt()
                     lastY = event.rawY.toInt()
                     Log.i("FloatingImage", "params: x: ${params.x}, y: ${params.y}")
-                    Log.i("FloatingImage", "x: ${lastX}, y: ${lastY}")
+                    Log.i("FloatingImage", "x: $lastX, y: $lastY")
                     true
                 }
                 MotionEvent.ACTION_MOVE -> {
@@ -93,35 +100,7 @@ class FloatingImage(context: Context) {
                 else -> false
             }
         }
-//        imageView.setOnTouchListener { _, event ->
-//            when (event.action) {
-//                MotionEvent.ACTION_DOWN -> {
-//                    lastX = event.rawX.toInt()
-//                    lastY = event.rawY.toInt()
-//                    true
-//                }
-//                MotionEvent.ACTION_MOVE -> {
-//                    val deltaX = (event.rawX - lastX).toInt()
-//                    val deltaY = (event.rawY - lastY).toInt()
-//                    params.x += deltaX
-//                    params.y += deltaY
-//
-//                    // Обновите параметры изображения
-//                    windowManager.updateViewLayout(imageView, params)
-//
-//                    // Обновите координаты изображения в ImageView
-//                    imageView.translationX = params.x.toFloat()
-//                    imageView.translationY = params.y.toFloat()
-//
-//
-//                    lastX = event.rawX.toInt()
-//                    lastY = event.rawY.toInt()
-//
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
+
 
     }
 
